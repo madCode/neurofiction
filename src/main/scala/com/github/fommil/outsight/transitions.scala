a22930fa40be61dec215f54798be0074cd293b7e
Sam Halliday <sam.halliday@gmail.com>
2013-02-26 22:42:13 +0000

    matcher sexiness

--- src/main/scala/com/github/fommil/outsight/transitions.scala
+++ src/main/scala/com/github/fommil/outsight/transitions.scala
@@ -12,23 +12,24 @@ class SnowWhiteTransitions extends TransitionCalculator {
   private def scene(name: String) = Scene(s"classpath:com/github/fommil/outsight/snowwhite/$name.md}")
 
   def next(journey: Journey) =
-    if (journey.scenes.isEmpty) scene("intro")
-    else journey.scenes.last._1.id match {
-      case "intro" => scene("dreaming-princess")
-      case "dreaming-princess" => scene("life1")
-      case "life1" => scene("death1")
-      case "death1" => scene("kiss")
-      case "kiss" => scene("queen")
-      case "queen" => scene("dwarfs") // [sic]
-      case "dwarfs" => scene("hunter")
-      case "hunter" =>
-        // TODO: decide if snow white lives or dies
-        val last = journey.scenes.last
-        ???
+    journey.scenes.reverse match {
+      case Nil => scene("intro")
+      case last :: before => last._1.id match {
+        case "intro" => scene("dreaming-princess")
+        case "dreaming-princess" => scene("life1")
+        case "life1" => scene("death1")
+        case "death1" => scene("kiss")
+        case "kiss" => scene("queen")
+        case "queen" => scene("dwarfs") // [sic]
+        case "dwarfs" => scene("hunter")
+        case "hunter" =>
+          // TODO: decide if snow white lives or dies
+          ???
 
-      case "ending-death" => Fin
-      case "ending-life" => Fin
-      case _ => throw new IllegalArgumentException
+        case "ending-death" => Fin
+        case "ending-life" => Fin
+        case _ => throw new IllegalArgumentException
+      }
     }
 
 }
