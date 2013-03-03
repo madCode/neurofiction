e4e027cf4c8a9bbb9695114ef4645a331beb0a07
Sam Halliday <sam.halliday@gmail.com>
2013-03-03 21:49:00 +0000

    code cleanliness

--- src/main/scala/com/github/fommil/outsight/transitions.scala
+++ src/main/scala/com/github/fommil/outsight/transitions.scala
@@ -8,14 +8,16 @@ trait TransitionCalculator {
 
 case class SnowWhiteRules() extends TransitionCalculator with EmotivHistRestriction {
 
-  def scene(name: String) = Scene(s"classpath:com/github/fommil/outsight/snowwhite/$name.md")
+  private def scene(name: String) = Scene(s"classpath:com/github/fommil/outsight/snowwhite/$name.md")
+
+  private def nameFor(pair: (Scene, Set[Response])) = pair._1.resource.split("/").last.replace(".md", "")
 
   def restriction = Set() + scene("life1") + scene("death1")
 
   def next(journey: Journey) =
     journey.scenes.reverse match {
       case Nil => scene("intro")
-      case last :: before => last._1.resource.split("/").last.replace(".md", "") match {
+      case last :: before => nameFor(last) match {
         case "intro" => scene("dreaming-princess")
         case "dreaming-princess" => scene("life1")
         case "life1" => scene("death1")
