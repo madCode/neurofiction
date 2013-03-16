f9a10aded9b56404f4072cd9651419c187f323f5
Sam Halliday <sam.halliday@gmail.com>
2013-03-16 20:07:47 +0000

    outsource the fullscreen code

--- src/main/scala/com/github/fommil/outsight/Main.scala
+++ src/main/scala/com/github/fommil/outsight/Main.scala
@@ -1,7 +1,7 @@
 package com.github.fommil.outsight
 
-import java.awt.{Window, GraphicsEnvironment}
 import akka.contrib.jul.JavaLogging
+import com.github.fommil.swing.SwingConvenience
 
 object Main extends App with JavaLogging {
 
@@ -9,19 +9,11 @@ object Main extends App with JavaLogging {
   val story = Story(Set(EmotivHistExtractor(rules)), rules)
 
   val view = new StoryView(story, finished)
-  fullscreen(view)
+  SwingConvenience.fullscreen(view)
 
   def finished(journey: Journey, scene: Scene) {
     val latest = (scene, Set[Response]()) // dummy
     view.update(journey.copy(scenes = journey.scenes :+ latest))
-  }
-
-  @deprecated("move to fommil.common")
-  def fullscreen(window: Window) {
-    GraphicsEnvironment.getLocalGraphicsEnvironment.getDefaultScreenDevice.setFullScreenWindow(view)
-    // http://stackoverflow.com/questions/13064607
-    view.setVisible(false)
-    view.setVisible(true)
   }
 
   //  val emotiv = new Emotiv()
